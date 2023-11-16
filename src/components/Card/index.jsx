50df643ce9a5e26b40c92ba7bd850b047edfdbaa
Pablo Smolak <pablosmolak2004@hotmail.com>
2023-11-15 21:33:49 -0400

    ajustando os paddings

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -8,7 +8,7 @@ export default function Card({ id, titulo, dataInicio, dataFim, imagem,local}){
                 <div className={styles.card}>
                     <img src={`${imagem}`} className={styles.imagens}/>
                     <div className={styles.info}>
-                        <h5 className={styles.data}><p>{dataInicio}</p><p style={{color: "#A9A9A9"}}>{">"}</p><p>{dataFim}</p></h5>
+                        <h5 className={styles.data}><p>{dataInicio}</p><p style={{color: "#A9A9A9", "font-weight": "bold", "font-size": "20px"}}>{">"}</p><p>{dataFim}</p></h5>
                         <h2 className={styles.cardTitulo}>{titulo}</h2>
                         <h3 className={styles.local}>{local}</h3>
                     </div>
